551ed7c7ace4425d66e14ef82326a05110bad200
Pavel Smelov <pavsmel@mail.ru>
2017-12-05 23:57:26 +0300

    Update solve.go

--- task5/solve.go
+++ task5/solve.go
@@ -41,14 +41,36 @@ func GetItems(w http.ResponseWriter, r *http.Request){
   w.WriteHeader(http.StatusOK)
   w.Write(j)
 }
+
+func UpdateItems(w http.ResponseWriter, r *http.Request){
+  vars := mux.Vars(r)
+  k, err := strconv.Atoi(vars["key"])
+  if err != nil {
+    panic(err)
+  }
+  var itemToUpdate myItem
+  err = json.NewDecoder(r.Body).Decode(&itemToUpdate)
+  if err != nil{
+    panic(err)
+  }
+  if item, ok := itemsStore[k]; ok {
+    itemToUpdate.key = item.key
+    delete(itemsStore, k)
+    itemsStore[k] = itemToUpdate
+  }
+  w.WriteHeader(http.StatusNoContent)
+}
+
 func (m *messageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, m.message)
 }
+
 func main(){
     ///mux := http.NewServeMux()
     router := mux.NewRouter()
     apiRouter := router.PathPrefix("solve").Subrouter()
     apiRouter.HandleFunc("/",GetItems).Methods("GET")
+    apiRouter.HandleFunc("/{key}", UpdateItems).Methods("UPDATE")
     //apiRouter.HandleFunc("/",AddTodoItems).Methods("POST")
     //fs := http.FileServer(http.Dir("solve"))
     //mux.Handle("/", fs)
